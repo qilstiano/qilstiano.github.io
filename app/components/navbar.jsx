10fe31a5f4c34468d26fe075caaac7551e054e44
a'qil <131641361+qilstiano@users.noreply.github.com>
2024-08-21 16:41:42 +0800

    misc

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -5,9 +5,9 @@ import styles from './Navbar.module.css';
 function Navbar() {
   return (
     <div className={styles.navbar}>
-      <Link legacyBehavior href="/"><a className={styles.navButton}>My Work</a></Link>
-      <Link legacyBehavior href="/process"><a className={styles.navButton}>My Process</a></Link>
-      <Link legacyBehavior href="/about"><a className={styles.navButton}>About Me</a></Link>
+      <Link legacyBehavior href="/"><a className={styles.navButton}>About Me</a></Link>
+      <Link legacyBehavior href="/process"><a className={styles.navButton}>My Work</a></Link>
+      <Link legacyBehavior href="/about"><a className={styles.navButton}>My Process</a></Link>
       <Link legacyBehavior href="/blog"><a className={styles.navButton}>Blog</a></Link>
       <Link legacyBehavior href="/contact"><a className={styles.navButton}>Contact Me</a></Link>
     </div>
