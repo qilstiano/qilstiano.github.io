2603ef0b3a3ed834867ffe5178c029ee83bde95c
a'qil <131641361+qilstiano@users.noreply.github.com>
2025-01-25 03:32:36 +0800

    bug/build-time-error

--- app/components/LoadingScreen.jsx
+++ app/components/LoadingScreen.jsx
@@ -1,7 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react';
 import { gsap } from 'gsap';
-import 'tailwindcss/tailwind.css';
 
 const LoadingScreen = () => {
   const words = ["WELCOME", "TO", "THE", "BEST", "PORTFOLIO", "EVER", "!!!!!!"];
