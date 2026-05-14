acd5576d45e2113e1f7b1e4eed6e97b9fede007d
a'qil <131641361+qilstiano@users.noreply.github.com>
2026-05-14 18:32:45 +0800

    update

--- app/App.tsx
+++ app/App.tsx
@@ -7,7 +7,7 @@ function App() {
     // Handle resume download
     const handleResumeDownload = () => {
         const link = document.createElement('a');
-        link.href = '/resume.pdf';
+        link.href = '/my_freakin_resume.pdf';
         link.download = 'my_freakin_resume.pdf';
         document.body.appendChild(link);
         link.click();
@@ -25,20 +25,39 @@ function App() {
             <section className="hero-section">
                 <div className="hero-content">
                     <div className="greeting-wrapper">
-                        <span className="greeting-line">hi! i'm A'qil</span>
+                        <span className="greeting-line">hey!</span>
                     </div>
                     <div className="bio">
                         <p>
-                            i'm a Computer Science undergraduate from the National University of Singapore.
+                            hello i'm A'qil! i'm a Computer Science undergraduate from the National University of Singapore.
                             i like cybersecurity and very much prefer to be building things/contributing
-                            to projects that actually matter to people, not for profits. i'm proudest of my work when it has some
-                            positive impact on people/society and isn't some pointless LLM wrapper or the openclaw slop that you see everywhere on LinkedIn.
-                            my proudest work (so far) is the work i do over at <a href="https://www.jalanjourney.com" target="_blank" rel="noopener noreferrer" className="inline-link">Jalan Journey</a>. i also like cats :)
+                            to projects that actually matter to people and not motivated by profits. 
                         </p>
+                        <br></br>
+                        <p>
+                            i'm proudest of my work when it has some
+                            positive impact on people/society and isn't some pointless LLM wrapper or the openclaw slop that you see everywhere on LinkedIn.
+                            my proudest work (so far) is the work i do over at <a href="https://www.jalanjourney.com" target="_blank" rel="noopener noreferrer" className="inline-link">Jalan Journey</a>. as the CTO, i lead a pretty talented team of software engineers in building products
+                            to better education across ASEAN and beyond. i'm beyond lucky to have the opportunity to do what i love & value add to the education of so many while i'm at it.
+                        </p>
+                        <br></br>
+                        <p>
+                            i've interned as a Security Services intern over at <a href="https://www.mufg.jp/english/index.html" target="_blank" rel="noopener noreferrer" className="inline-link">MUFG</a> and as a Founding Backend Engineer at Score Financial. 
+                            these weren't ideal roles for me as, well, they basically do nothing for the masses of society but i gotta put food on my table. i'm optimistic that i'll come across some work in more
+                            socially beneficial sectors/projects in the future. i did learn a lot about full-stack engineering and security though from those opportunities. win some, lose some i guess.
+                        </p>
+                        <br></br>
+                        <p>
+                            beyond the boring nerdy stuff, i enjoy torturing myself by running. before i run, i perform my usual rounds around the neighbourhood to inspect the stray cats to see if they're still as manja
+                            as usual. i enjoy reading too, usually on anything as long as i can look intellectual when i quote it in conversations/arguments. i attempt to sound verbose on a wide range of 
+                            topics spanning tech, politics and life in my <a href='https://www.notion.so/Ruhi-2844ee3a19d680609058ce40b9cdb6e3?source=copy_link' target="_blank" rel="noopener noreferrer" className="inline-link">blog</a>. i have a number of hobbies that come and go too, some interesting ones are: graphic design, 3D art and falling asleep at ungodly hours. 
+                        </p>
+                        <br></br>
+                        <span className="farewell-line">have a nice day!</span>
                     </div>
                     <div className="social-links">
                         <div className="kaypoh-line">
-                            <span className="kaypoh-text">go and kaypoh me here:</span>
+                            <span className="kaypoh-text">go and kaypoh more here:</span>
                             <div className="kaypoh-icons">
                                 <a href="https://github.com/qilstiano" target="_blank" rel="noopener noreferrer">
                                     <FaGithub /> github
